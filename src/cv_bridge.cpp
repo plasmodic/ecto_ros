75865b0430143cc334049bec4f288f38f23450e4
Ethan Rublee <ethan.rublee@gmail.com>
2011-09-08 17:17:20 -0700

    Adding time tweaks so that time does not depend on ros::init being called, Also a bit more cv bridge stuffs.  Most likely need to move this out of ecto_ros.

--- src/cv_bridge.cpp
+++ src/cv_bridge.cpp
@@ -39,8 +39,24 @@
 #include <iostream>
 #include <string>
 
+#include <ros/ros.h>
+
 namespace
 {
+  void
+  header_stanza(std_msgs::Header& header)
+  {
+    header.seq++;
+    if (ros::isInitialized())
+    {
+      header.stamp = ros::Time::now();
+    }
+    else
+    {
+      ros::WallTime w = ros::WallTime::now();
+      header.stamp = ros::Time(w.sec,w.nsec);
+    }
+  }
   namespace enc = sensor_msgs::image_encodings;
   int
   getCvType(const std::string& encoding)
@@ -285,8 +301,8 @@ namespace ecto_ros
       {
         image_msg->encoding = *encoding_;
       }
-      header_.seq++;
-      header_.stamp = ros::Time::now();
+
+      header_stanza(header_);
       image_msg->header = header_;
       *cloud_msg_out_ = image_msg;
       return ecto::OK;
@@ -330,8 +346,7 @@ namespace ecto_ros
     {
       CloudPtr cloud_msg(new PointCloudT());
       toPointCloud(*mat_, *cloud_msg);
-      header_.seq++;
-      header_.stamp = ros::Time::now();
+      header_stanza(header_);
       cloud_msg->header = header_;
       *cloud_msg_out_ = cloud_msg;
       return ecto::OK;
@@ -343,10 +358,13 @@ namespace ecto_ros
     ecto::spore<std::string> encoding_;
   };
 
-  struct Mat2PointCloud : Mat2PointCloud_<sensor_msgs::PointCloud> {};
+  struct Mat2PointCloud: Mat2PointCloud_<sensor_msgs::PointCloud>
+  {
+  };
 
-  struct Mat2PointCloud2 : Mat2PointCloud_<sensor_msgs::PointCloud2> {};
-
+  struct Mat2PointCloud2: Mat2PointCloud_<sensor_msgs::PointCloud2>
+  {
+  };
 
 }
 
--- src/cv_camera_info.cpp
+++ src/cv_camera_info.cpp
@@ -32,6 +32,7 @@
 #include <opencv2/core/core.hpp>
 #include <iostream>
 #include <string>
+#include <ros/ros.h>
 
 namespace ecto_ros
 {
@@ -68,7 +69,7 @@ namespace ecto_ros
       cv::Mat D(ci.D.size(), 1, CV_64FC1);
       for (size_t i = 0; i < ci.D.size(); i++)
       {
-        D.at<double>(i, 1) = ci.D[i];
+        D.at<double>(i) = ci.D[i];
       }
       cv::Size s(ci.width, ci.height);
       *image_size_ = s;
@@ -79,5 +80,77 @@ namespace ecto_ros
     ecto::spore<cv::Mat> K_, D_;
     ecto::spore<cv::Size> image_size_;
   };
+
+  struct Cv2CameraInfo
+  {
+    static void
+    declare_params(tendrils& p)
+    {
+      p.declare<std::string>("frame_id");
+    }
+    static void
+    declare_io(const tendrils& p, tendrils& i, tendrils& o)
+    {
+      i.declare<cv::Mat>("K");
+      i.declare<cv::Mat>("D");
+      i.declare<cv::Size>("image_size");
+      o.declare<CameraInfoConstPtr>("camera_info");
+    }
+    void
+    configure(const tendrils& p, const tendrils& i, const tendrils& o)
+    {
+      frame_id_ = p["frame_id"];
+      K_ = i["K"];
+      D_ = i["D"];
+      image_size_ = i["image_size"];
+      camera_info_ = o["camera_info"];
+    }
+    int
+    process(const tendrils&, const tendrils&)
+    {
+      CameraInfoPtr ci(new CameraInfo);
+      ci->header.frame_id = *frame_id_;
+      ci->header.seq++;
+      if (ros::isInitialized())
+      {
+        ci->header.stamp = ros::Time::now();
+      }
+      else
+      {
+        ros::WallTime w = ros::WallTime::now();
+        ci->header.stamp = ros::Time(w.sec, w.nsec);
+      }
+      cv::Mat K;
+      K_->convertTo(K, CV_64F); //convert to double
+      for (int i = 0; i < 9; i++)
+        ci->K[i] = K.at<double>(i / 3, i % 3);
+
+      cv::Mat D;
+      D_->convertTo(D, CV_64F);
+
+      if (!D.empty())
+      {
+        ci->D.resize(D.rows);
+        for (int i = 0; i < D.rows; i++)
+        {
+          ci->D[i] = D.at<double>(0, i);
+        }
+      }
+      ci->R[0] = ci->R[4] = ci->R[8] = 1;
+      ci->P[0] = ci->P[5] = ci->P[9] = 1;
+
+      ci->width = image_size_->width;
+      ci->height = image_size_->height;
+      *camera_info_ = ci;
+      return ecto::OK;
+    }
+    ecto::spore<CameraInfoConstPtr> camera_info_;
+    ecto::spore<cv::Mat> K_, D_;
+    ecto::spore<cv::Size> image_size_;
+    ecto::spore<std::string> frame_id_;
+  };
 }
-ECTO_CELL(ecto_ros, ecto_ros::CameraInfo2Cv, "CameraInfo2Cv", "Takes a CameraInfo message and converts to OpenCV types.");
+ECTO_CELL(ecto_ros, ecto_ros::CameraInfo2Cv, "CameraInfo2Cv",
+          "Takes a CameraInfo message and converts to OpenCV types.");
+ECTO_CELL(ecto_ros, ecto_ros::Cv2CameraInfo, "Cv2CameraInfo",
+          "Takes opencv style camera info, and converts to an CameraInfo message.");
