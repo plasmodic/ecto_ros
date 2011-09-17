eb3284a72de661006aa1e5844ab66c4f7f815520
Ethan Rublee <ethan.rublee@gmail.com>
2011-09-17 10:53:01 -0700

    Clean up the sync_sub sample a bit, with comments.

--- samples/sync_sub.py
+++ samples/sync_sub.py
@@ -1,45 +1,41 @@
 #!/usr/bin/env python
-PKG = 'ecto_ros' # this package name
+PKG = 'ecto_ros'
 import roslib; roslib.load_manifest(PKG)
 import ecto
 import ecto_ros, ecto_sensor_msgs
-from ecto_opencv import highgui
-import sys
+from ecto_opencv.highgui import imshow
 
 ImageSub = ecto_sensor_msgs.Subscriber_Image
-CameraInfoSub = ecto_sensor_msgs.Subscriber_CameraInfo
+
 def do_ecto():
+    #Set up the ecto_ros subscribers, with a dict of output name -> subscriber cell
+    subs = dict(image=ImageSub(topic_name='image',queue_size=0),
+                depth=ImageSub(topic_name='depth',queue_size=0),
+                )
+    #The synchronizer expects this dict, and will have an output foreach key:value pair
+    sync = ecto_ros.Synchronizer('Synchronizator', subs=subs)
 
-    subs = dict( image=ImageSub(topic_name='/camera/rgb/image_color',queue_size=0),
-                depth=ImageSub(topic_name='/camera/depth/image',queue_size=0),
-                depth_info=CameraInfoSub(topic_name='/camera/depth/camera_info',queue_size=0),
-                image_info=CameraInfoSub(topic_name='/camera/rgb/camera_info',queue_size=0),
-             )
+    #some rosmsg ~> opencv type converters
+    rgb = ecto_ros.Image2Mat()
+    depth = ecto_ros.Image2Mat()
     
-    sync = ecto_ros.Synchronizer('Synchronizator', subs=subs
-                                 )
-
-    drift_printer = ecto_ros.DriftPrinter()
-    
-    im2mat_rgb = ecto_ros.Image2Mat()
-    im2mat_depth = ecto_ros.Image2Mat()
-    
+    #declare a graph that just shows the images using opencv highgui
     graph = [
-                sync["image"] >> im2mat_rgb["image"],
-                im2mat_rgb["image"] >> highgui.imshow("rgb show", name="rgb")[:],
-                sync[:] >> drift_printer[:],
-                sync["depth"] >> im2mat_depth["image"],
-                im2mat_depth["image"] >> highgui.imshow("depth show", name="depth")[:]
+                sync["image"] >> rgb["image"],
+                sync["depth"] >> depth["image"],
+                rgb["image"] >> imshow(name="rgb")["image"],
+                depth["image"] >> imshow(name="depth")["image"]
             ]
+    
+    #add the graph to our ecto plasm
     plasm = ecto.Plasm()
     plasm.connect(graph)
-    ecto.view_plasm(plasm)
     
+    #We'll use a threadpool based scheduler to execute this one.
     sched = ecto.schedulers.Threadpool(plasm)
-    sched.execute()
+    sched.execute() #execute forever
 
-    #sched = ecto.schedulers.Singlethreaded(plasm)
-    #sched.execute()
 if __name__ == "__main__":
-    ecto_ros.init(sys.argv, "ecto_node")
+    import sys
+    ecto_ros.init(sys.argv, "ecto_node") #strips out ros remappings and initializes roscpp
     do_ecto()
