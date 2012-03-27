30f09c9c04fe97dc1333af6d69f8a10e580d7cc2
Vincent Rabaud <vrabaud@willowgarage.com>
2012-03-26 23:49:22 -0700

    forgot the setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Ecto ROS',
+      version='1.0.0',
+      description='Ecto bindings for ROS',
+      packages=['ecto_ros'],
+      package_dir={'':'python'}
+)
