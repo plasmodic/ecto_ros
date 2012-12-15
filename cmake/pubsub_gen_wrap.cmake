b9d015b4f572797afc492cf87f286ffcc13e0b6d
Vincent Rabaud <vrabaud@willowgarage.com>
2012-12-15 01:49:39 +0100

    fix the renaming of genmsg

--- cmake/pubsub_gen_wrap.cmake
+++ cmake/pubsub_gen_wrap.cmake
@@ -42,7 +42,7 @@ macro(pubsub_gen_wrap ROS_PACKAGE)
     if (ROS_FUERTE_FOUND)
     find_package(ROS REQUIRED gencpp genmsg roscpp rosbag)
     else()
-    find_package(catkin REQUIRED gencpp genmsg roscpp rosbag)
+    find_package(catkin REQUIRED gencpp message_generation roscpp rosbag)
     endif()
 
   find_program(ECTO_ROS_GEN_MSG_WRAPPERS
